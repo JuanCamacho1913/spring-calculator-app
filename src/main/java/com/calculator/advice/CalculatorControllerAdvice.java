c2797b2d0ad8edb46aaef77113c71438288b89ad
¨juan <jecb1913@gmail.com>
2025-05-05 21:07:51 -0500

    Arrangement of operations by date and type of operation and readme

--- src/main/java/com/calculator/advice/CalculatorControllerAdvice.java
+++ src/main/java/com/calculator/advice/CalculatorControllerAdvice.java
@@ -1,10 +1,13 @@
 package com.calculator.advice;
 
+import com.calculator.exception.error.ArithmeticOperationException;
+import com.calculator.exception.error.ElementNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -46,5 +49,23 @@ public class CalculatorControllerAdvice {
         response.put("message", "Invalid operation parameters");
         response.put("details", List.of(ex.getMessage()));
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ElementNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> elementNotFoundException(ElementNotFoundException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", HttpStatus.BAD_REQUEST.value());
+        response.put("message", "Element not found");
+        response.put("details", List.of(ex.getMessage()));
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> generalException(Exception ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        response.put("message", "Unknown error");
+        response.put("details", List.of(ex.getMessage()));
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
